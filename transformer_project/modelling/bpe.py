8fb12a37e781014a0290ab2659e553400f2680d6
Nils Reck <reck@sipgate.de>
2024-10-19 12:47:56 +0200

    :feat: implement bpe (draft)

--- transformer_project/modelling/bpe.py
+++ transformer_project/modelling/bpe.py
@@ -15,10 +15,54 @@ text = text.lower()
 
 words = text.split()
 
-word_counter = Counter()
 word_counter = Counter(words)
 
 vocabulary = []
 for s in text:
-    if s not in vocabulary and s != ' ':
+    if s not in vocabulary and s != " ":
         vocabulary.extend(s)
+
+splits = {word: [c for c in word] for word in word_counter.keys()}
+
+
+def compute_pair_freqs(splits):
+    pair_freqs = Counter()
+    for word, freq in word_counter.items():
+        split = splits[word]
+        if len(split) == 1:
+            continue
+        for i in range(len(split) - 1):
+            pair = (split[i], split[i + 1])
+            pair_freqs[pair] += freq
+    return pair_freqs
+
+
+pair_freqs = compute_pair_freqs(splits)
+
+rules = {}
+
+
+def create_rule(pair_freqs, rules, vocabulary):
+    most_common_pair = pair_freqs.most_common(1)[0]
+    concatenated = "".join(most_common_pair[0])
+    rules[most_common_pair[0]] = concatenated
+    vocabulary.append(concatenated)
+    pair_freqs.pop(most_common_pair[0])
+
+
+
+def replace_in_splits(splits, rules):
+    for _, split in splits.items():
+        i = 0
+        while i < len(split) - 1:
+            pair = (split[i], split[i + 1])
+            if pair in rules:
+                split[i : i + 2] = [rules[pair]]
+            i += 1
+
+while len(vocabulary) < 64:
+    pair_freqs = compute_pair_freqs(splits)
+    create_rule(pair_freqs, rules, vocabulary)
+    replace_in_splits(splits, rules)
+
+print(vocabulary)
