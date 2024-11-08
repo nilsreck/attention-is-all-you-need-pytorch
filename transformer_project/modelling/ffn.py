f5cf33674216a006b08383842fe8a3e6c2ef5bdc
Nils Reck <reck@sipgate.de>
2024-11-08 10:47:10 +0100

    :sparkles: implement position-wise feed-forward network

---
+++ transformer_project/modelling/ffn.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ffn: int):
+        super().__init__()
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(d_model, d_ffn), nn.ReLU(), nn.Linear(d_ffn, d_model)
+        )
+
+        def forward(self, x):
+            return self.feed_forward(x)
