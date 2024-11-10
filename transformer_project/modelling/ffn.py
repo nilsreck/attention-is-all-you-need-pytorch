77784d7635251a70f039b0e3536444710c0c4c60
Nils Reck <reck@sipgate.de>
2024-11-10 13:40:27 +0100

    :white_check_mark: pass encoder layer test

--- transformer_project/modelling/ffn.py
+++ transformer_project/modelling/ffn.py
@@ -5,7 +5,6 @@ class PositionWiseFeedForward(nn.Module):
     def __init__(self, d_model: int, d_ffn: int):
         super().__init__()
 
-        super().__init__()
         self.linear1 = nn.Linear(d_model, d_ffn)
         self.relu = nn.ReLU()
         self.linear2 = nn.Linear(d_ffn, d_model)
--- transformer_project/modelling/functional.py
+++ transformer_project/modelling/functional.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class BaseTransformerLayer(nn.Module):
-    def __init__(self, input_dim, num_heads, feature_dim, dropout):
+    def __init__(self, input_dim, num_heads, feature_dim, dropout=0.0):
         super().__init__()
         self.input_dim = input_dim
 
@@ -17,11 +17,12 @@ class BaseTransformerLayer(nn.Module):
         self.feature_transformation = PositionWiseFeedForward(input_dim, feature_dim)
         self.layer_norm_2 = nn.LayerNorm(input_dim)
 
-    def forward(self):
-        attention_output = self.self_attention(input)
+    def forward(self, input, attention_mask):
+        attention_output = self.self_attention(input, input, input, attention_mask)
         residual_connections_mha = attention_output + input
         normalized_mha_output = self.layer_norm_1(residual_connections_mha)
         ffn_outputs = self.feature_transformation(normalized_mha_output)
         residual_connections_ffn = ffn_outputs + normalized_mha_output
+        final = self.layer_norm_2(residual_connections_ffn)
 
-        return residual_connections_ffn
+        return final
