a786682186c3e0102a0ae06ad48471853cb51279
Nils Reck <reck@sipgate.de>
2024-11-10 12:44:23 +0100

    :construction: implement encoder layer

---
+++ transformer_project/modelling/functional.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import os
+import sys
+from modelling.attention import MultiHeadAttention
+from modelling.ffn import PositionWiseFeedForward
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class BaseTransformerLayer(nn.Module):
+    def __init__(self, input_dim, num_heads, feature_dim, dropout):
+        super().__init__()
+        self.input_dim = input_dim
+
+        self.self_attention = MultiHeadAttention(input_dim, num_heads)
+        self.layer_norm_1 = nn.LayerNorm(input_dim)
+        self.feature_transformation = PositionWiseFeedForward(input_dim, feature_dim)
+        self.layer_norm_2 = nn.LayerNorm(input_dim)
+
+    def forward(self):
+        attention_output = self.self_attention(input)
+        residual_connections_mha = attention_output + input
+        normalized_mha_output = self.layer_norm_1(residual_connections_mha)
+        ffn_outputs = self.feature_transformation(normalized_mha_output)
+        residual_connections_ffn = ffn_outputs + normalized_mha_output
+
+        return residual_connections_ffn
