e3a5bfa70f35dceacdfe2688fd2794fe2680387c
Nils Reck <reck@sipgate.de>
2024-11-10 13:18:32 +0100

    :recycle: remove Sequential container to comply with encoder test

--- transformer_project/modelling/ffn.py
+++ transformer_project/modelling/ffn.py
@@ -5,9 +5,13 @@ class PositionWiseFeedForward(nn.Module):
     def __init__(self, d_model: int, d_ffn: int):
         super().__init__()
 
-        self.feed_forward = nn.Sequential(
-            nn.Linear(d_model, d_ffn), nn.ReLU(), nn.Linear(d_ffn, d_model)
-        )
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ffn)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(d_ffn, d_model)
 
-        def forward(self, x):
-            return self.feed_forward(x) 
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return x
