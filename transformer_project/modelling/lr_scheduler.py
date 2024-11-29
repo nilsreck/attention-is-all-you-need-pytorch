36ce66d050596ea3838e08aad8e48b793e5e6223
Nils Reck <reck@sipgate.de>
2024-11-29 10:31:08 +0100

    :sparkles: implement lr scheduler

---
+++ transformer_project/modelling/lr_scheduler.py
@@ -0,0 +1,24 @@
+import torch
+from typing import Optimizer
+
+
+class LR_Scheduler:
+    def __init__(self, optimizer: Optimizer, d_model: int, warmup_steps: int):
+        self.optimizer = optimizer
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
+        self.step_num = 0
+        self.last_lr = 0
+
+        def step(self) -> float:
+            """Perform a step."""
+            result = (self.d_model**-0.5) * min(
+                self.step_num ** (-0.5), self.step_num * self.warmup_steps ** (-1.5)
+            )
+            self.step_num += 1
+            self.last_lr = result
+            return result
+
+        def get_lr(self) -> float:
+            """Return last computed learning rate."""
+            return self.last_lr
