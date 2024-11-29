40bd585beed39d04facf23c0d2bd71f8baef1ed8
Nils Reck <reck@sipgate.de>
2024-11-29 15:28:48 +0100

    :white_check_mark: adjust tolerance

--- transformer_project/test/practical_6.py
+++ transformer_project/test/practical_6.py
@@ -203,4 +203,4 @@ def test_layer(layer, input, encoder, encoder_attention_mask, attention_mask, ex
     # Mask padded positions
     actual *= attention_mask.unsqueeze(-1).float()
 
-    assert torch.allclose(actual, expected)
+    assert torch.allclose(actual, expected, rtol=1e-4)
