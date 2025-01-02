17912b52a5610bbcac71ada3b9106a1d1ff3af44
Nils Reck <reck@sipgate.de>
2025-01-02 14:56:02 +0100

    :sparkles: align encoded sequences

--- transformer_project/data/translation_dataset.py
+++ transformer_project/data/translation_dataset.py
@@ -7,15 +7,11 @@ class TranslationDataset(Dataset):
         self,
         dataset,
         tokenizer,
-        source_lang="de",
-        target_lang="en",
         transform=None,
-        max_len=32,
+        max_len=64,
     ):
         self.dataset = dataset
         self.tokenizer = tokenizer
-        self.source_lang = source_lang
-        self.target_lang = target_lang
         self.transform = transform
         self.max_len = max_len
 
@@ -23,9 +19,13 @@ class TranslationDataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        source_text = self.dataset[idx][self.source_lang]
-        target_text = self.dataset[idx][self.target_lang]
-        source_tokens = torch.tensor(
+        source_text = self.dataset[idx]["de"]
+        target_text = self.dataset[idx]["en"]
+
+        bos_token_id = self.tokenizer.bos_token_id
+        eos_token_id = self.tokenizer.eos_token_id
+
+        encoded_source = torch.tensor(
             self.tokenizer.encode(
                 source_text,
                 max_length=self.max_len,
@@ -33,17 +33,27 @@ class TranslationDataset(Dataset):
                 truncation=True,
             )
         )
-        target_tokens = torch.tensor(
+
+        encoded_target = torch.tensor(
             self.tokenizer.encode(
                 target_text,
-                max_length=self.max_len,
+                max_length=self.max_len - 1,
                 padding="max_length",
                 truncation=True,
             )
         )
 
-        if self.transform:
-            source_tokens = self.transform(source_tokens)
-            target_tokens = self.transform(target_tokens)
+        encoder_target_input = torch.cat([torch.tensor([bos_token_id]), encoded_target])
+        encoder_target_output = torch.cat(
+            [encoded_target, torch.tensor([eos_token_id])]
+        )
 
-        return source_tokens, target_tokens
+        if self.transform:
+            encoded_source = self.transform(encoded_source)
+            encoder_target_output = self.transform(encoder_target_output)
+
+        return {
+            "source": encoded_source,
+            "target_input": encoder_target_input,
+            "target_output": encoder_target_output,
+        }
